53cd4141d1725132e83f8ac7e71be39a9207b2d2
Lyu Pengfei <lv.pengfei@oriental-pearl.cn>
2017-09-07 14:35:32 +0800

    add

--- test/dist/page/t1.js
+++ test/dist/page/t1.js
@@ -10,44 +10,50 @@ class Main {
         this.stage = new createjs.Stage(canvas);
         this.stage.scaleX = stageScale;
         this.stage.scaleY = stageScale;
-        createjs.Touch.enable(this.stage);
         this.container = new createjs.Container();
         this.message = new createjs.Text(document.documentElement.clientWidth, 'bold 30px Segoe UI', '#e66000');
         this.message.textAlign = 'left';
         this.message.x = 100;
         this.message.y = 200;
         this.container.addChild(this.message);
-        let shape = new createjs.Shape();
-        let g = shape.graphics;
+        this.shape = new createjs.Shape();
+        let g = this.shape.graphics;
         g.beginFill('#d030f0');
         g.drawRect(0, 0, 375, 100);
         g.endFill();
-        shape.x = 0;
-        shape.y = 0;
-        shape.snapToPixel = true;
-        this.stage.addChild(shape);
+        this.shape.x = 0;
+        this.shape.y = 0;
+        this.shape.snapToPixel = true;
+        this.stage.addChild(this.shape);
         this.stage.addChild(this.container);
+        createjs.Touch.enable(this.stage);
         createjs.Ticker.addEventListener('tick', (e) => this.tick(e));
+        this.testShake();
     }
     tick(e) {
-        /*if (stageWidth != document.documentElement.clientWidth || stageHeight != document.documentElement.clientHeight) {
-            stageWidth = document.documentElement.clientWidth;
-            stageHeight = document.documentElement.clientHeight;
-            //外部元素自适应
-            canvas.width = stageWidth;
-            canvas.height = stageHeight;
-
-            //内部元素自适应
-            stageScale = stageWidth / 750;//宽度自适应；
-//    stageScale = stageHeight/1206;//高度自适应两者选一
-            container.scaleX = stageScale;
-            container.scaleY = stageScale;
-//    bitmap.x = (stageWidth -  750*bitmap.scaleX)/2;//高度自适应时解开这个注释 保证图片居中
-            if (leftBtn) {
-                leftBtn.x = stageWidth - 100;
-            }
-        }*/
+        if (this.shape.y < 10) {
+            this.shape.y += 2;
+        }
+        else if (this.shape.y > 0) {
+            console.log(this.shape.y);
+            this.shape.y += -2;
+        }
         this.stage.update(e);
+    }
+    testShake() {
+        //create a new instance of shake.js.
+        let myShakeEvent = new Shake({
+            threshold: 15
+        });
+        // start listening to device motion
+        myShakeEvent.start();
+        // register a shake event
+        window.addEventListener('shake', shakeEventDidOccur, false);
+        //shake event callback
+        function shakeEventDidOccur() {
+            //put your own code here etc.
+            alert('Shake!');
+        }
     }
 }
 window.addEventListener('load', () => {
--- test/src/page/t1.ts
+++ test/src/page/t1.ts
@@ -5,6 +5,7 @@ class Main {
 	private manifest: any[];
 	private queue: createjs.LoadQueue;
 	private message: createjs.Text;
+	private shape :createjs.Shape;
 
 	constructor(canvas: HTMLCanvasElement) {
 		this.canvas = canvas;
@@ -19,7 +20,6 @@ class Main {
 		this.stage = new createjs.Stage(canvas);
 		this.stage.scaleX = stageScale;
 		this.stage.scaleY = stageScale;
-		createjs.Touch.enable(this.stage);
 
 		 this.container = new createjs.Container();
 
@@ -33,42 +33,53 @@ class Main {
 		this.container.addChild(this.message);
 
 
-		let shape = new createjs.Shape();
-		let g = shape.graphics;
+		this.shape = new createjs.Shape();
+		let g = this.shape.graphics;
 		g.beginFill('#d030f0');
 		g.drawRect(0, 0, 375 , 100);
 		g.endFill();
-		shape.x = 0;
-		shape.y = 0;
-		shape.snapToPixel = true;
-		this.stage.addChild(shape);
+		this.shape.x = 0;
+		this.shape.y = 0;
+		this.shape.snapToPixel = true;
+		this.stage.addChild(this.shape);
 
 
 		this.stage.addChild(this.container);
+
+
+		createjs.Touch.enable(this.stage);
 		createjs.Ticker.addEventListener('tick', (e: createjs.TickerEvent) => this.tick(e));
 
+		this.testShake();
 
 	}
 
 	tick(e: createjs.TickerEvent) {
-		/*if (stageWidth != document.documentElement.clientWidth || stageHeight != document.documentElement.clientHeight) {
-			stageWidth = document.documentElement.clientWidth;
-			stageHeight = document.documentElement.clientHeight;
-			//外部元素自适应
-			canvas.width = stageWidth;
-			canvas.height = stageHeight;
-
-			//内部元素自适应
-			stageScale = stageWidth / 750;//宽度自适应；
-//    stageScale = stageHeight/1206;//高度自适应两者选一
-			container.scaleX = stageScale;
-			container.scaleY = stageScale;
-//    bitmap.x = (stageWidth -  750*bitmap.scaleX)/2;//高度自适应时解开这个注释 保证图片居中
-			if (leftBtn) {
-				leftBtn.x = stageWidth - 100;
-			}
-		}*/
+		if(this.shape.y < 10){
+			this.shape.y += 2;
+		}
+		else if(this.shape.y > 0){
+			console.log(this.shape.y);
+			this.shape.y += -2;
+		}
 		this.stage.update(e);
+	}
+
+
+	testShake(){
+		//create a new instance of shake.js.
+		let myShakeEvent = new Shake({
+			threshold: 15
+		});
+		// start listening to device motion
+		myShakeEvent.start();
+		// register a shake event
+		window.addEventListener('shake', shakeEventDidOccur, false);
+		//shake event callback
+		function shakeEventDidOccur () {
+			//put your own code here etc.
+			alert('Shake!');
+		}
 	}
 }
 
