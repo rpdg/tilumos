edce80431f6abf301e216dd6be2731a31b4284f6
Lyu Pengfei <lv.pengfei@oriental-pearl.cn>
2017-09-07 10:52:56 +0800

    add

--- test/dist/page/t1.js
+++ test/dist/page/t1.js
@@ -3,28 +3,28 @@ class Main {
         this.canvas = canvas;
         this.canvas.width = document.documentElement.clientWidth;
         this.canvas.height = document.documentElement.clientHeight;
-        let scale = 1 / window.devicePixelRatio;
+        //let scale = 1 / window.devicePixelRatio;
         let stageScale = document.documentElement.clientWidth / 750; //宽度自适应；
         //document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=device-width,initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
         this.stage = new createjs.Stage(canvas);
+        this.stage.scaleX = stageScale;
+        this.stage.scaleY = stageScale;
         createjs.Touch.enable(this.stage);
         this.container = new createjs.Container();
-        // this.container.scaleX = stageScale;
-        // this.container.scaleY = stageScale;
         this.message = new createjs.Text(document.documentElement.clientWidth, 'bold 30px Segoe UI', '#e66000');
-        this.message.textAlign = 'center';
-        this.message.x = canvas.width / 2;
-        this.message.y = canvas.height / 2;
+        this.message.textAlign = 'left';
+        this.message.x = 100;
+        this.message.y = 200;
         this.container.addChild(this.message);
         let shape = new createjs.Shape();
         let g = shape.graphics;
         g.beginFill('#d0b000');
-        g.drawCircle(100, 100, 30);
+        g.drawRect(0, 0, 100, 100);
         g.endFill();
-        shape.x = 50;
-        shape.y = 50;
+        shape.x = 100;
+        shape.y = 100;
         shape.snapToPixel = true;
-        //this.stage.addChild(shape);
+        this.stage.addChild(shape);
         this.stage.addChild(this.container);
         createjs.Ticker.addEventListener('tick', (e) => this.tick(e));
     }
