83860fc1ba8101ea51d33da36732904116e02ea4
Lyu Pengfei <lv.pengfei@oriental-pearl.cn>
2017-09-20 14:03:19 +0800

    add

---
+++ Bll/Authen.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: lv.pengfei
+ * Date: 2017/9/20
+ * Time: 12:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Web;
+using System.Web.Security;
+
+namespace admin.Bll
+{
+	/// <summary>
+	/// Description of Login.
+	/// </summary>
+	public static class Authen
+	{
+		public static int SignIn(string name, string password)
+		{
+			if(FormsAuthentication.Authenticate(name, password)) {
+			//if (Membership.ValidateUser(name, password)) {
+				var ticket = new FormsAuthenticationTicket("ticket", true, 24 * 3600 * 1000);
+				string encTicket = FormsAuthentication.Encrypt(ticket);
+				var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encTicket);
+				cookie.Path = FormsAuthentication.FormsCookiePath;
+				cookie.Domain = FormsAuthentication.CookieDomain;
+			
+				HttpContext.Current.Response.Cookies.Add(cookie);
+				return 1;
+			}
+			
+			//throw new UnauthorizedAccessException("用户名或密码不正确");
+			return 0 ;
+			
+		}
+	}
+}
--- Bll/ClickNote.cs
+++ Bll/ClickNote.cs
@@ -13,7 +13,7 @@ using System.Data.Common;
 using System.Collections.Generic;
 
 
-using Lyu;
+using Lyu.Text;
 using Lyu.Data;
 using Lyu.Data.Helper;
 using Lyu.Data.Types;
@@ -32,19 +32,83 @@ namespace admin.Bll
 		/*
  			* http://127.0.0.1:8011/api/admin/Bll.ClickNoteBll/GetPage
 		*/
-		public static DataPage GetPage( int pageIndex = 0 , int pageSize = 10)
+		public static DataPage GetPage(int pageIndex = 0, int pageSize = 10)
 		{
 
 			var wheres = new Dictionary<string, object>() {
 				
 			};
-			var which = new QueryObj
-            {
-                //Select = "exhibition_id,exhibition_name,type,time,address,limits,post_time,registration_count,attendance_count,bonus",
-                SortOn = "id",
-                SortType = "DESC"
-            };
+			
+			
+			var which = new QueryObj {
+				//Select = "exhibition_id,exhibition_name,type,time,address,limits,post_time,registration_count,attendance_count,bonus",
+				SortOn = "id",
+				SortType = "DESC"
+			};
+			
+			
 			return dal.Get(wheres, which, pageSize, pageIndex);
+		}
+		
+		
+		public static long Notice(string btnName)
+		{
+						
+			var nt = new ClickNote {
+				btnName = btnName,
+				ip = GetUserIP(),
+				time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+				browser = GetUserBrowser(),
+			};
+			
+			
+			var id = dal.Add<ClickNote>(nt);
+			
+			return id;
+		}
+		
+		private static string GetUserIP()
+		{
+			System.Web.HttpContext context = System.Web.HttpContext.Current; 
+			string ipList = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+			if (!string.IsNullOrEmpty(ipList)) {
+				return ipList.Split(',')[0];
+			}
+
+			return context.Request.ServerVariables["REMOTE_ADDR"];
+		}
+		
+		
+		private static string GetUserBrowser()
+		{
+			System.Web.HttpContext context = System.Web.HttpContext.Current; 
+			System.Web.HttpBrowserCapabilities browser = context.Request.Browser;
+			StringBuffer s = //"Type = " + browser.Type + "; " +
+			            "Name = " + browser.Browser + "; "
+						+ "Version = " + browser.Version + "; " ;
+			
+//			            + "Major Version = " + browser.MajorVersion + "\n"
+//			            + "Minor Version = " + browser.MinorVersion + "\n"
+//			            + "Platform = " + browser.Platform + "\n"
+//			            + "Is Beta = " + browser.Beta + "\n"
+//			            + "Is Crawler = " + browser.Crawler + "\n"
+//			            + "Is AOL = " + browser.AOL + "\n"
+//			            + "Is Win16 = " + browser.Win16 + "\n"
+//			            + "Is Win32 = " + browser.Win32 + "\n"
+//			            + "Supports Frames = " + browser.Frames + "\n"
+//			            + "Supports Tables = " + browser.Tables + "\n"
+//			            + "Supports Cookies = " + browser.Cookies + "\n"
+//			            + "Supports VBScript = " + browser.VBScript + "\n"
+//			            + "Supports JavaScript = " +
+//			            browser.EcmaScriptVersion.ToString() + "\n"
+//			            + "Supports Java Applets = " + browser.JavaApplets + "\n"
+//			            + "Supports ActiveX Controls = " + browser.ActiveXControls
+//			            + "\n"
+//			            + "Supports JavaScript Version = " +
+//			            browser["JavaScriptVersion"] + "\n";
+
+			return s;
 		}
 	}
 	
@@ -66,5 +130,8 @@ namespace admin.Bll
 	{
 		public long? id { set; get; }
 		public string btnName { set; get; }
+		public string ip { set; get; }
+		public string time { set; get; }
+		public string browser { set; get; }
 	}
 }
